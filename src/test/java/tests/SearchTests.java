d71d535c9d46b7d8da2e2c3f46e001c3e13417b4
anavla777 <w.ananenkow@gmail.com>
2024-07-16 18:16:14 +0300

    small fixes in Step names

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
 
+@DisplayName("Search tests")
 public class SearchTests extends TestBase {
 
     @Test
@@ -40,9 +41,10 @@ public class SearchTests extends TestBase {
             $(accessibilityId("Search Wikipedia")).click();
             $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Java");
         });
-        step("open an article", () ->
+        step("Open an article", () ->
                 $(id("org.wikipedia.alpha:id/page_list_item_title"))
                         .click());
-        $(className("android.widget.TextView")).shouldHave(text("Java"));;
+        step("Verify that article has mentioned text", () ->
+                $(className("android.widget.TextView")).shouldHave(text("Java")));
     }
 }
