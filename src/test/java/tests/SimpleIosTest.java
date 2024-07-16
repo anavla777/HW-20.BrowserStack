e541d868ffb739cd869339f1745de5fe9bda4a5e
anavla777 <w.ananenkow@gmail.com>
2024-07-16 21:30:11 +0300

    configured IOS test app

---
+++ src/test/java/tests/SimpleIosTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.appium.SelenideAppium.$;
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.qameta.allure.Allure.step;
+
+public class SimpleIosTest extends TestBase{
+    @Test
+    @Tag("ios")
+    @DisplayName("Text enter test")
+    void inputTextTest()  {
+        step("Click on Text button", () -> {
+            $(accessibilityId("Text Button")).click();
+        });
+        step("Input text", () -> {
+            $(accessibilityId("Text Input")).sendKeys("Simple IOS Test"+"\n");
+        });
+        step("Check text output", () -> {
+            $(accessibilityId("Text Output")).shouldHave(text("Simple IOS Test"));
+        });
+    }
+}
